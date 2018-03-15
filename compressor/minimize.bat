92b835a5bdaf3e812947907c182cb3ec6e6f7a6a
Alexander Whipp <alexruns4@gmail.com>
2018-03-15 01:21:54 -0400

    Removed minimized files

--- compressor/minimize.bat
+++
@@ -1,6 +0,0 @@
-echo "Compressing Javascript..."
-java -jar yui.jar ../js/route-calculator.js -o ../js/route-calculator.min.js
-java -jar yui.jar ../js/init.js -o ../js/init.min.js
-
-echo "Compressing Stylesheets..."
-java -jar yui.jar ../css/styles.css -o ../css/styles.min.css
--- compressor/minimize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Compressing Javascript..."
-java -jar yui.jar ../js/route-calculator.js -o ../js/route-calculator.min.js
-java -jar yui.jar ../js/init.js -o ../js/init.min.js
-
-echo "Compressing Stylesheets..."
-java -jar yui.jar ../css/styles.css -o ../css/styles.min.css
