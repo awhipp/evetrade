1bc7e4689997b976a7e1ffb324d605b36cafbe3a
Damien Gaignon <damien.gaignon@gmail.com>
2020-02-03 21:03:06 +0100

    Add a clean button on station hauling
    The button appear for 5 stations or more in the list.

--- js/init.js
+++ js/init.js
@@ -332,8 +332,11 @@ function newStartStation(e) {
                 var data = $(this)[0].previousSibling.data;
                 addedToStartList.splice(addedToStartList.indexOf(data), 1);
                 $(this.parentElement).remove();
+                if (addedToStartList.length < 5) $("#s2s_start_clean").hide();
             }
         }
+
+        if (addedToStartList.length >= 5) $("#s2s_start_clean").show();
     }
 
 }
@@ -394,8 +397,11 @@ function newEndStation(e) {
                 var data = $(this)[0].previousSibling.data;
                 addedToEndList.splice(addedToEndList.indexOf(data), 1);
                 $(this.parentElement).remove();
+                if (addedToEndList.length < 5) $("#s2s_end_clean").hide();
             }
         }
+
+        if (addedToEndList.length >= 5) $("#s2s_end_clean").show();
     }
 }
 
@@ -840,4 +846,18 @@ function init(style){
         return false;
     }
     return false;
+}
+
+function cleanStartList() {
+    $("#s2s_route_start").empty();
+    $("#s2s_route_start").hide();
+    addedToStartList = [];
+    $("#s2s_start_clean").hide();
+}
+
+function cleanEndList() {
+    $("#s2s_route_end").empty();
+    $("#s2s_route_end").hide();
+    addedToEndList = [];
+    $("#s2s_end_clean").hide();
 }
