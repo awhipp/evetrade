f7da4b3f5bc0c92a4bb084bcf2c3e404296f082d
Alexander Whipp <alexruns4@gmail.com>
2022-07-18 23:10:22 -0400

    Fixed indent

--- js/station-hauling.js
+++ js/station-hauling.js
@@ -8,10 +8,10 @@ let universeList = {};
 let hauling_request = {};
 
 /**
- * Generic function to get JSON data from API endpoint
- * @param {*} fileName 
- * @returns The Data from the API.
- */
+* Generic function to get JSON data from API endpoint
+* @param {*} fileName 
+* @returns The Data from the API.
+*/
 function getResourceData(fileName) {
     return fetch(API_ENDPOINT + '/resource?file=' + fileName)
     .then(response => response.json())
@@ -21,16 +21,16 @@ function getResourceData(fileName) {
 }
 
 /**
- * Get's the station list data for the EVE universe.
- * @returns {Promise<void>}
- */
+* Get's the station list data for the EVE universe.
+* @returns {Promise<void>}
+*/
 function getStationList(){
     return new Promise (function(resolve, reject) {
         const lastRetrieved = window.localStorage.getItem('evetrade_station_list_last_retrieved');
-
+        
         if(dateString == lastRetrieved) {
             console.log('Same Day - Retrieving Resource Cache.');
-
+            
             try {
                 resolve(JSON.parse(window.localStorage.getItem('stationList')));
             } catch(e) {
@@ -39,30 +39,30 @@ function getStationList(){
         } else {
             console.log('New Day - Retrieving StationList Cache.');
         }
-
+        
         window.localStorage.removeItem('evetrade_station_list_last_retrieved')
-
+        
         getResourceData('stationList.json').then(function(response) {
             console.log('Station List Loaded.');
             window.localStorage.setItem('stationList', JSON.stringify(response));
             resolve(response);
         });
     });
-
+    
 }
 
 
 /**
- * Get's the universe list data for the EVE universe.
- * @returns {Promise<void>}
- */
+* Get's the universe list data for the EVE universe.
+* @returns {Promise<void>}
+*/
 function getUniverseList() {
     return new Promise (function(resolve, reject) {
         const lastRetrieved = window.localStorage.getItem('evetrade_universe_list_last_retrieved');
-
+        
         if(dateString == lastRetrieved) {
             console.log('Same Day - Retrieving Resource Cache.');
-
+            
             try {
                 resolve(JSON.parse(window.localStorage.getItem('universeList')));
             } catch(e) {
@@ -71,28 +71,28 @@ function getUniverseList() {
         } else {
             console.log('New Day - Retrieving UniverseList Cache.');
         }
-
+        
         window.localStorage.removeItem('evetrade_universe_list_last_retrieved')
-
+        
         getResourceData('universeList.json').then(function(response) {
             console.log('Universe List Loaded.');
             window.localStorage.setItem('universeList', JSON.stringify(response));
             resolve(response);
         });
     });
-
+    
 }
 
 function addStationToList(stationName, domId) {
     const data = universeList[stationName.toLowerCase()];
     const dataAttribute = `#${domId} li[data-station="${data.station}"]`;
-
+    
     let stationList = $(`#${domId}`);
-
+    
     if ($(dataAttribute).length == 0) {
         stationList.show();
         stationList.append(`<li data-region="${data.region}" data-station="${data.station}"><span class="stationName">${stationName}</span><span class="remove-item">x</span></li>`);
-
+        
         $(`#${domId} li`).on("click", function() {
             const parent = $(this.parentElement);
             $(this).remove();
@@ -127,10 +127,10 @@ function getStationNamesFromList(domId) {
 }
 
 /**
- * Initializes the auto complete function for the given input.
- * @param {} domId The id of the input to initialize. 
- * @param {*} list  The list of options to use for the auto complete.
- */
+* Initializes the auto complete function for the given input.
+* @param {} domId The id of the input to initialize. 
+* @param {*} list  The list of options to use for the auto complete.
+*/
 function initAwesomplete(domId, list) {
     var input = document.querySelector("#" + domId);
     var inputPlete  = new Awesomplete(input, {
@@ -142,7 +142,7 @@ function initAwesomplete(domId, list) {
         filter: Awesomplete.FILTER_STARTSWITH,
         sort: false,
     });
-        
+    
     $(input).on('awesomplete-select', function(selection) {
         console.log(`Added (select): ${selection.originalEvent.text.value}`);
         addStationToList(selection.originalEvent.text.value, selection.target.id + 'Stations');
@@ -152,28 +152,28 @@ function initAwesomplete(domId, list) {
 }
 
 function createTradeHeader(request, from, to) {
-
+    
     const minProfit = request.minProfit;
     const maxWeight = request.maxWeight == Number.MAX_SAFE_INTEGER ? "Infinite" : request.maxWeight;
     const minROI = (request.minROI * 100) + "%";
     const maxBudget = request.maxBudget == Number.MAX_SAFE_INTEGER ? "Infinite" : request.maxBudget;
     const tax = (request.tax * 100) + "%";
-
+    
     let systemSecurity = request.systemSecurity;
     switch(systemSecurity) {
         case "high_sec,low_sec,null_sec":
-            systemSecurity = "High, Low, Null";
+        systemSecurity = "High, Low, Null";
         case "high_sec,low_sec":
-            systemSecurity = "High, Low";
+        systemSecurity = "High, Low";
         default:
-            systemSecurity = "High";
+        systemSecurity = "High";
     }
-
+    
     const routeSafety = request.routeSafety.replace('secure', 'Safest').replace('insecure', 'Least Safe').replace('shortest', 'Shortest');
-
+    
     let subHeader = `Profit Above: ${minProfit} | Capacity: ${maxWeight} | R.O.I.: ${minROI}`
     subHeader += `<br><br>Budget: ${maxBudget} | Sales Tax: ${tax} | Security: ${systemSecurity} | Route: ${routeSafety}`;
-
+    
     $('main h1').hide();
     $('main h2').html(`Buying from: ${from} <br>Selling to: ${to}`);
     $('main h3').html(subHeader);
@@ -183,7 +183,7 @@ function getNameFromUniverseStations(stationId) {
     if (stationId.indexOf(':') >= 0) {
         stationId = stationId.split(':')[1];
     }
-
+    
     for (const stationName in universeList) {
         if (universeList[stationName].station == stationId) {
             return universeList[stationName];
@@ -194,31 +194,31 @@ function getNameFromUniverseStations(stationId) {
 }
 
 /**
- * Pulls data from form HTML element and creates JSON
- */
+* Pulls data from form HTML element and creates JSON
+*/
 async function getHaulingData(hasQueryParams) {
     let from = [];
     let to = [];
-
+    
     if (hasQueryParams) {
         // Converting From/To Query Params back to station names.
         fromLocations = hauling_request.from.split(',');
         for(const flocation of fromLocations) {
             from.push(getNameFromUniverseStations(flocation.split(':')[1]).name)
         }
-
+        
         toLocations = hauling_request.to.split(',');
         for(const tlocation of toLocations) {
             to.push(getNameFromUniverseStations(tlocation.split(':')[1]).name)
         }
-
+        
         from = from.join(',');
         to = to.join(',');
     } else {
         console.log('Pulling from Form');
         from = getStationNamesFromList('fromStations');
         to = getStationNamesFromList('toStations');
-
+        
         hauling_request = {
             from: getStationInfoFromList('fromStations'),
             to: getStationInfoFromList('toStations'),
@@ -231,30 +231,30 @@ async function getHaulingData(hasQueryParams) {
             tax: parseFloat((parseFloat($("#tax").val()) || 0.08).toFixed(2))
         }
     }
-
+    
     console.log(hauling_request);
-
+    
     const qs = Object.keys(hauling_request)
-        .map(key => `${key}=${hauling_request[key]}`)
-        .join('&');
-
+    .map(key => `${key}=${hauling_request[key]}`)
+    .join('&');
+    
     if (history.pushState) {
         var newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${qs}`;
         window.history.pushState({path:newurl},'',newurl);
     }
-
+    
     createTradeHeader(hauling_request, from, to);
-
+    
     const qp = new URLSearchParams(hauling_request).toString();
     const requestUrl = `${API_ENDPOINT}/hauling?${qp}`;
     const timerHandler = `${new Date().getTime()} Hauling Request.`;
-
+    
     console.time(timerHandler);
-
+    
     $("#hauling-form").fadeOut(function(){
         $("#hauling-form").remove();
     });
-
+    
     return fetch(requestUrl)
     .then(response => response.json())
     .then(function(response) {
@@ -275,20 +275,20 @@ function createTable(data) {
     $(".dataTableFilters").html("");
     
     var dataTableDOM = $("#dataTable");
-
+    
     const columns = [{data: "View", name: "View"}];
-
+    
     let sort_column = 1;
     let idx = 1;
     let hidden_columns = [];
-
+    
     initial_hidden = window.localStorage.getItem('evetrade_station_col_preferences');
     if (initial_hidden == null) {
         initial_hidden = ["Item ID", "Net Costs", "Net Sales", "Gross Margin", "Sales Taxes", "Jumps", "Profit per Jump"];
     } else {
         initial_hidden = initial_hidden.split(',');
     }
-
+    
     $.each(data[0], function(name, value) {
         if (name != "View") {
             columns.push({data: name, title: name});
@@ -298,10 +298,10 @@ function createTable(data) {
         if (initial_hidden.includes(name)) hidden_columns.push(idx);
         idx += 1;
     });
-
+    
     // Append to colvis as we loop
     // <li><button class="colvistoggle btn btn-default is-true" colidx="1">Sell Order</button></li>
-
+    
     const dt = dataTableDOM.DataTable({
         "order": [[sort_column, "desc"]],
         "lengthMenu": [[50], ["50"]],
@@ -317,23 +317,23 @@ function createTable(data) {
         columns: columns,
         data: data
     });
-
+    
     $("label > input").addClass("form-control").addClass("minor-text");
     $("label > input").attr("placeholder", "Filter by Station or Item");
-
+    
     $(".dataTableFilters").append($("#dataTable_filter"));
     $(".dataTableFilters").append($(".dt-buttons"));
     $(".dt-button").addClass("btn");
     $(".dt-button").addClass("btn-default");
-
+    
     $(".dataTables_paginate").on("click", function(){
         $(this)[0].scrollIntoView();
     });
-
+    
     $(".dataTable tr").on('click',function() {
         $(this).toggleClass('row_selected');
     });
-
+    
     
     $(".colvistoggle").on('click',function(e) {
         e.preventDefault();
@@ -348,10 +348,10 @@ function createTable(data) {
         } else {
             initial_hidden.splice(initial_hidden.indexOf($(this).text()), 1);
         }
-
+        
         window.localStorage.setItem('evetrade_station_col_preferences', initial_hidden.join(','));
     });
-
+    
     for (let i = 0; i < hidden_columns.length; i++) {
         $(`[colidx="${hidden_columns[i]}"]`).removeClass('is-true');
         $(`[colidx="${hidden_columns[i]}"]`).addClass('is-false');
@@ -372,7 +372,7 @@ function swapTradeHub(stationName) {
         case "Amarr VIII (Oris) - Emperor Family Academy":
             return "Amarr";
         default:
-            return stationName
+            return stationName;
     }
 }
 
@@ -380,25 +380,25 @@ function swapTradeHub(stationName) {
 * Creates the datatable based on the trading style that is being queried
 */
 function displayData(data) {
-
+    
     data.forEach(function(row) {      
         from = swapTradeHub(row['From']['name']);
         to = swapTradeHub(row['Take To']['name']);
-
+        
         row['From'] = `<span class='${row['From']['security_code']}'>${from}</span>`;
         row['Take To'] = `<span class='${row['Take To']['security_code']}'>${to}</span>`;
         row['View'] = `<a class="investigate" href=
         '/orders?itemId=${row['Item ID']}&from=${hauling_request['from']}&to=${hauling_request['to']}' 
         target='_blank'><i class="fa fa-search-plus"></i></a>`;
     });
-
+    
     createTable(data);
 }
 
 function executeHauling(hasQueryParams) {
     $(".tableLoadingIcon").show();
     // createTradeHeader();
-
+    
     getHaulingData(hasQueryParams).then((data) => {
         if (data.length == 0) {
             $(".tableLoadingIcon").html(`No Results Found<br><a class="btn btn-grey btn-border btn-effect" href="javascript:window.location.replace(location.pathname);">Refresh this page</a>`);
@@ -409,30 +409,30 @@ function executeHauling(hasQueryParams) {
 }
 
 /**
- * Initializes on window load
- */
+* Initializes on window load
+*/
 function loadNext() {
     
     try {
         if (window.location.search.length > 0) {
             var search = window.location.search.substring(1);
             const thr = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
-
+            
             if (thr.from && thr.to && thr.maxBudget && thr.maxWeight && thr.minProfit && thr.minROI && thr.routeSafety && thr.systemSecurity && thr.tax) {
                 console.log("Found query params:");
                 console.log(thr);
                 hauling_request = thr;
-    
+                
                 getUniverseList().then(function(data) {
                     universeList = data;
                     executeHauling(true);
                 });
                 return;
             }
-
+            
             console.log(`Invalid search parameters: ${search}`);
         }
-
+        
     } catch(e) {
         console.log(`Error parsing query params ${location.search}.`);
     }
@@ -445,7 +445,7 @@ function loadNext() {
         });
         
         console.log(`${stationList.length} stations loaded.`);
-
+        
         initAwesomplete("from", "stationList");
         initAwesomplete("to", "stationList");
     });
@@ -453,8 +453,8 @@ function loadNext() {
     getUniverseList().then(function(data) {
         universeList = data;
     });
-
-        
+    
+    
     $("#submit").click(function(){
         // Form Validation
         if (getStationNamesFromList('fromStations') == "" || getStationNamesFromList('toStations') == "") {
@@ -465,7 +465,7 @@ function loadNext() {
             executeHauling(false);
         }
     });
-
+    
     const formElements = ['minProfit', 'maxWeight', 'minROI', 'maxBudget', 'tax', 'systemSecurity', 'routeSafety'];
     for (let i = 0; i < formElements.length; i++) {
         $(`#${formElements[i]}`).inputStore();
