b09975a79fa9b30f773a5dce8ed07515cbf3edba
Alexander Whipp <alexruns4@gmail.com>
2023-04-16 17:01:18 -0400

    Updating to refactored python lambda function

--- js/orders.js
+++ js/orders.js
@@ -130,7 +130,7 @@ let thr = {};
 * Initializes on window load
 */
 function loadNext() {
-    API_ENDPOINT = window.location.href.startsWith('https://evetrade.space') ? `${global_config['api_gateway']}/orders` : `${global_config['api_gateway']}/dev/orders`;
+    API_ENDPOINT = `${global_config['api_gateway']}/orders`;
 
     countDownDivText(functionDurations['evetrade-get-orders']);
     $(".tableLoadingIcon").show();
