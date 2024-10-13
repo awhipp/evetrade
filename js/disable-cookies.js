a32e91c84d7aca399c1c2771097510be7d62861c
Alexander Whipp <alexruns4@gmail.com>
2024-10-13 14:44:38 -0400

    Update disable-cookies.js

--- js/disable-cookies.js
+++ js/disable-cookies.js
@@ -1,4 +1,4 @@
-/*const date = new Date();
+const date = new Date();
 const dateString = "Date=" + date.getFullYear() + date.getMonth() + date.getDate() + date.getHours();
 
 // Disable cookies for this site entirely
@@ -6,12 +6,12 @@ document.cookie = "cookieconsent_status=deny; expires=Thu, 01 Jan 1970 00:00:00 
 
 (function() {
     const originalCookieProperty = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie') || Object.getOwnPropertyDescriptor(HTMLDocument.prototype, 'cookie');
-    if (originalCookieProperty && originalCookieProperty.configurable) {
+/*    if (originalCookieProperty && originalCookieProperty.configurable) {
         Object.defineProperty(document, 'cookie', {
             get: function() {
                 return ''; // Return empty string for cookie reads
             },
             set: function(value) {} // No-op for cookie writes
         });
-    }
-})();*/
+    }*/
+})();*
