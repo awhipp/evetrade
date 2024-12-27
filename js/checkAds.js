da2f0b74ad2583f4e0be65cae8a5640c68d8d062
Alexander Whipp <alexruns4@gmail.com>
2024-12-27 18:04:15 -0500

    feat: :sparkles: Refactor Google Funding Choices integration by moving script logic to checkAds.js

---
+++ js/checkAds.js
@@ -0,0 +1,8 @@
+window.setTimeout(
+    function(){            
+        if (!window.frames['googlefcPresent']) {
+            console.log('Google Funding Choices not present...');
+        }
+        (function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();
+    }, 5000
+);
